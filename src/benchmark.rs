bc0f499b1cb2e40faddcafc34a9bd6107c0b6850
Robin Lange <robin.langenc@gmail.com>
2020-07-01 21:25:03 +1000

    Moved benchmark code out of main

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -15,33 +15,33 @@ fn main() -> Result<()> {
     let path_str = std::env::args().skip(1).next().unwrap();
     let path = Path::new(&path_str);
 
-    benchmark(&path)?;
-
-    Ok(())
-}
-
-fn benchmark(path: &Path) -> Result<()> {
-
     for category_path in fs::read_dir(path)? {
 
         let category_path = category_path?.path();
         let category_path_str = category_path.to_str().ok_or(anyhow!("Not unicode"))?;
-        println!("{}", category_path_str);
+        println!("\n{}", category_path_str);
 
         for dicom_path in fs::read_dir(category_path)? {
 
             let dicom_path = dicom_path?.path();
-            let dicom_path_str = dicom_path.to_str().ok_or(anyhow!("Not unicode"))?;
-
-            let dicom = open_file(dicom_path.as_os_str())?;
+            let filename = dicom_path
+                .file_name().ok_or(anyhow!("Incorrect folder structure"))?
+                .to_str().ok_or(anyhow!("Not unicode"))?;
 
             let t0 = Instant::now();
+            let dicom = open_file(dicom_path.as_os_str())?;
+            let t1 = Instant::now();
             let decoded_image_data = get_image(dicom)?;
             let DecodedImageData { pixel_data, .. } = decoded_image_data;
+            let t2 = Instant::now();
 
-            let dt = (t0.elapsed().as_millis() as f32) / 1000.0;
+            let dt1 = ((t1 - t0).as_millis() as f32) / 1000.0;
+            let dt2 = ((t2 - t1).as_millis() as f32) / 1000.0;
 
-            println!("\t{} : {}s  ({} bytes)", dicom_path_str, dt, pixel_data.len());
+            println!("  {}", filename);
+            println!("    {} bytes", pixel_data.len());
+            println!("    Opening  : {}s", dt1);
+            println!("    Decoding : {}s", dt2);
         }
     }
 
---
+++ src/dicom2png.rs
@@ -0,0 +1,25 @@
+use std::path::PathBuf;
+use dicom::object::open_file;
+use anyhow::{Result, anyhow};
+
+mod utils;
+mod decoding;
+
+use utils::write_image;
+use decoding::get_image;
+
+fn main() -> Result<()> {
+
+    let mut args = std::env::args().skip(1);
+
+    let input_path = PathBuf::from(
+        args.next().ok_or(anyhow!("Not enough arguments"))?);
+    let output_path = PathBuf::from(
+        args.next().ok_or(anyhow!("Not enough arguments"))?);
+
+    let dicom = open_file(input_path.as_os_str())?;
+    let decoded_image_data = get_image(dicom)?;
+    write_image(&decoded_image_data, output_path.as_path())?;
+
+    Ok(())
+}
